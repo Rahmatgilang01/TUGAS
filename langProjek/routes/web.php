2cb0eb05d90d7b7192b583116e7bea972320c68e
Rahmatgilang01 <rahmatgilangperdana669@gmail.com>
2024-05-25 23:12:29 +0700

    Tugas

---
+++ langProjek/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Homecontroller;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/',[Homecontroller::class, 'index']);
+
+Route::get('/register',[AuthController::class, 'register']);
+
+route::post('/welcome',[AuthController::class,'welcome']);
